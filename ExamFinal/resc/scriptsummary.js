b6fe19d333b84e39cd61fbf6c5a00cca1a81bdcb
Ricky Paian Limbong <50484063+qee20@users.noreply.github.com>
2025-07-09 13:00:24 +0700

    additional file

---
+++ ExamFinal/resc/scriptsummary.js
@@ -0,0 +1,20 @@
+
+
+function formatRupiah(amount) {
+    return 'Rp ' + amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+}
+
+// Calculate order total
+function calculateTotal(finalchoize) {
+    
+}
+
+// Handle form submission
+document.getElementById('customerForm').addEventListener('submit', function (e) {
+   
+});
+
+// Generate invoice function
+function generateInvoice(customerData) {
+   
+}
